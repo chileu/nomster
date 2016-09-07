29635a7b44ee8453a7147b9fb871aefc8e2d3bc7
chiyingleung1 <chiying.leung@gmail.com>
2016-09-07 14:09:09 +0000

    Added carrierwave require line to environment.rb file

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,5 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
-
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params)
@@ -10,7 +9,7 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end  
 
 end
